02c1672a4c999a762865d9d4915d3a0c242fd910
Joseph Bursey <jbursey@tevora.com>
2026-07-10 16:49:12 -0700

    Start json parsing for workflows

--- pkg/fam/action.go
+++ pkg/fam/action.go
@@ -8,22 +8,22 @@ import (
 )
 
 type Payload struct {
-    Id      string                // The string to replace in the template
-    Type    fact.TargetType        // What does this payload turn the target into?
-    Pl      string                // The actual payload
+    Id      string              // The string to replace in the template
+    Type    fact.TargetType     // What does this payload turn the target into?
+    Pl      string              // The actual payload
 }
 
 type PayloadSet struct {
-    Id   string
-    Type fact.TargetType
-    File string
-    List []Payload
+    Id   string                 `json:"Id"`
+    Type fact.TargetType        `json:"Type"`
+    File string                 `json:"File"`
+    List []Payload              `json:"List"`
 }
 
 // Describes how to make the request
 type RequestTemplate struct {
-    Method string                // The Method to use (i.e. "GET, PUT, FUZZ")
-    Url    string                // The Url Template (i.e. "BASE/FUZZ")
+    Method string               `json:"Method"`
+    Url    string               `json:"UrlTemplate"`
     //Header *HeaderTemplate
     //Body   *BodyTemplate
 }
@@ -39,8 +39,8 @@ type ResponseAction struct {
 }
 
 type Action struct {
-    Id      job.Action
-    Pylds   *PayloadSet
-    Reqt    *RequestTemplate
-    RespAct *ResponseAction
+    Id      job.Action          `json:"Id"`
+    Pylds   *PayloadSet         `json:"PayloadSet"`
+    Reqt    *RequestTemplate    `json:"RequestTemplate"`
+    RespAct *ResponseAction     `json:"ResponseAction"`
 }
