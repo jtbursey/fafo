f32cc1ba363a9d5826acd8eaecce0b12072bbc09
jtbursey <josephtbursey@gmail.com>
2026-07-12 23:48:35 -0700

    Comply with the go extension for some reason

--- fafo/fafo.go
+++ fafo/fafo.go
@@ -3,162 +3,163 @@
 package main
 
 import (
-    "path/filepath"
-    "flag"
-    "fmt"
-    "net/http"
+	"flag"
+	"fmt"
+	"net/http"
+	"path/filepath"
 
-    "fafo/pkg/chrome"
-    "fafo/pkg/config"
-    "fafo/pkg/env"
-    "fafo/pkg/fact"
-    "fafo/pkg/fs"
-    "fafo/pkg/httpclient"
-    "fafo/pkg/job"
-    "fafo/pkg/log"
-    "fafo/pkg/pretty"
-    "fafo/pkg/worker"
+	"fafo/pkg/chrome"
+	"fafo/pkg/config"
+	"fafo/pkg/env"
+	"fafo/pkg/fact"
+	"fafo/pkg/fs"
+	"fafo/pkg/httpclient"
+	"fafo/pkg/job"
+	"fafo/pkg/log"
+	"fafo/pkg/pretty"
+	"fafo/pkg/worker"
 )
 
 // Define the command line args here
 var (
-    flagURL = flag.String("url", "", "The base `URL` (domain) to hit")
-    flagEP = flag.String("ep", "", "The specific `Endpoint` to hit (overrides URL)")
-    flagPort = flag.Int("p", 443, "The `Port` on which to scan")
-    flagOut = flag.String("o", config.DefaultFindingsDir, "The `Directory` in which to put the findings")
-    flagC = flag.String("c", "", "The `Config File` to use")
-    flagConfig = flag.String("config", config.DefaultConfigFile, "The `Config File` to use")
-    flagNoScrSh = flag.Bool("disable-screenshot", false, "Disable all screenshotting functionality")
+	flagURL     = flag.String("url", "", "The base `URL` (domain) to hit")
+	flagEP      = flag.String("ep", "", "The specific `Endpoint` to hit (overrides URL)")
+	flagPort    = flag.Int("p", 443, "The `Port` on which to scan")
+	flagOut     = flag.String("o", config.DefaultFindingsDir, "The `Directory` in which to put the findings")
+	flagC       = flag.String("c", "", "The `Config File` to use")
+	flagConfig  = flag.String("config", config.DefaultConfigFile, "The `Config File` to use")
+	flagNoScrSh = flag.Bool("disable-screenshot", false, "Disable all screenshotting functionality")
 )
 
 func Loop(env *env.Env) {
-    prefix := ""
-    if log.Verb(3) {
-        prefix = fmt.Sprintf("%*s", pretty.PrefixWidth, "[Manager]: ")
-    }
-    for {
-        select {
-        case t := <- env.FactCh:
-            t.PrintFacts(1, prefix)
-            env.Targets.Push(t)
-            // TODO: Output the existing URLs to findings dir
-        case j := <- env.JobCh:
-            env.Jobqueue.Push(j)
-        default:
-            if env.Jobqueue.Done() && len(env.FactCh) == 0 && len(env.JobCh) == 0 {
-                log.Logf(0, "%vAll jobs completed.\n", prefix)
-                return
-            }
-        }
-    }
+	prefix := ""
+	if log.Verb(3) {
+		prefix = fmt.Sprintf("%*s", pretty.PrefixWidth, "[Manager]: ")
+	}
+	for {
+		select {
+		case t := <-env.FactCh:
+			t.PrintFacts(1, prefix)
+			env.Targets.Push(t)
+			// TODO: Output the existing URLs to findings dir
+		case j := <-env.JobCh:
+			env.Jobqueue.Push(j)
+		default:
+			if env.Jobqueue.Done() && len(env.FactCh) == 0 && len(env.JobCh) == 0 {
+				log.Logf(0, "%vAll jobs completed.\n", prefix)
+				return
+			}
+		}
+	}
 }
 
 func main() {
-    flag.Parse()
-    
-    if *flagURL == "" && *flagEP == "" {
-        log.Err("No target was given! Please use either -url or -ep\n")
-        flag.PrintDefaults()
-        return
-    }
+	flag.Parse()
 
-    cfg := config.DefaultConfig()
-    if *flagC != "" {
-        cfg.SelfFile = *flagC
-    } else {
-        cfg.SelfFile = *flagConfig
-    }
-    
-    if err := cfg.Parse(); err != nil {
-        return
-    }
-    cfg.FindingsDir = *flagOut
+	// TODO: combine this into single url, then identify it automatically
+	// TODO: Convert my url strings into go urls
+	if *flagURL == "" && *flagEP == "" {
+		log.Err("No target was given! Please use either -url or -ep\n")
+		flag.PrintDefaults()
+		return
+	}
 
-    log.Greeting("By Ocelot")
+	cfg := config.DefaultConfig()
+	if *flagC != "" {
+		cfg.SelfFile = *flagC
+	} else {
+		cfg.SelfFile = *flagConfig
+	}
 
-    jq := &job.JobQueue{}
-    jq.Init()
+	if err := cfg.Parse(); err != nil {
+		return
+	}
+	cfg.FindingsDir = *flagOut
 
-    httpclient := httpclient.New(cfg.ClientCfg)
+	log.Greeting("By Ocelot")
 
-    env := &env.Env{
-        Jobqueue: *jq,
-        Cfg:      *cfg,
-        Client:   *httpclient,
-        ScrShCh:  make(chan http.Request, 10),
-        JobCh:    make(chan job.Job, 10),
-        FactCh:   make(chan fact.Target, 10),
-    }
+	jq := &job.JobQueue{}
+	jq.Init()
 
-    if err := env.ParseActions(); err != nil {
-        return
-    }
+	httpclient := httpclient.New(cfg.ClientCfg)
 
-    // Define the top-level target
-    firstTarget := &fact.Target{
-        Port:  *flagPort,
-        Facts: make(map[fact.FactKey]fact.FactValue),
-    }
-    if *flagEP != "" {
-        firstTarget.Url = *flagEP
-    } else {
-        firstTarget.Url = *flagURL
-    }
-    env.FirstTarget = *firstTarget
-    env.Targets.Push(*firstTarget)
+	env := &env.Env{
+		Jobqueue: *jq,
+		Cfg:      *cfg,
+		Client:   *httpclient,
+		ScrShCh:  make(chan http.Request, 10),
+		JobCh:    make(chan job.Job, 10),
+		FactCh:   make(chan fact.Target, 10),
+	}
 
-    // Create the first discovery job
-    firstJob := job.Job{
-        Mode:     job.ModeDiscovery,
-        Action:   "CheckAlive",
-        Priority: 5,
-        Target:   firstTarget.Key(),
-    }
+	if err := env.ParseActions(); err != nil {
+		return
+	}
 
-    if err := env.Validate(); err != nil{
-        log.Errf("Environment failed validation: %v\n", err)
-        return
-    }
+	// Define the top-level target
+	firstTarget := &fact.Target{
+		Port:  *flagPort,
+		Facts: make(map[fact.FactKey]fact.FactValue),
+	}
+	if *flagEP != "" {
+		firstTarget.Url = *flagEP
+	} else {
+		firstTarget.Url = *flagURL
+	}
+	env.FirstTarget = *firstTarget
+	env.Targets.Push(*firstTarget)
 
-    env.Debug()
+	// Create the first discovery job
+	firstJob := job.Job{
+		Mode:     job.ModeDiscovery,
+		Action:   "CheckAlive",
+		Priority: 5,
+		Target:   firstTarget.Key(),
+	}
 
-    if err := fs.Mkdir(env.Cfg.FindingsDir); err != nil {
-        log.Errf("Failed to create directory %v: %v\n", env.Cfg.FindingsDir, err)
-        return
-    }
+	if err := env.Validate(); err != nil {
+		log.Errf("Environment failed validation: %v\n", err)
+		return
+	}
 
-    var chrm *chrome.Chrome
-    if !*flagNoScrSh {
-        env.Cfg.ScrShDir = filepath.Join(env.Cfg.FindingsDir, "screenshots")
-        if err := fs.Mkdir(env.Cfg.ScrShDir); err != nil {
-            log.Errf("Failed to mkdir %v: %v", env.Cfg.ScrShDir, err)
-            return
-        }
+	env.Debug()
 
-        chrm = chrome.NewChrome(env)
-        if chrm == nil {
-            log.Err("Failed to launch Chrome!")
-            return
-        }
-        go chrm.Loop(env)
-    } else {
-        log.Log(0, "Screenshotting is Disabled\n")
-        env.Cfg.DisableScreenShot = true
-    }
+	if err := fs.Mkdir(env.Cfg.FindingsDir); err != nil {
+		log.Errf("Failed to create directory %v: %v\n", env.Cfg.FindingsDir, err)
+		return
+	}
 
-    // Spawn the Worker Threads
-    for i := uint(0); i < env.Cfg.NumWorkers; i++ {
-        go worker.Run(i, env)
-    }
+	var chrm *chrome.Chrome
+	if !*flagNoScrSh {
+		env.Cfg.ScrShDir = filepath.Join(env.Cfg.FindingsDir, "screenshots")
+		if err := fs.Mkdir(env.Cfg.ScrShDir); err != nil {
+			log.Errf("Failed to mkdir %v: %v", env.Cfg.ScrShDir, err)
+			return
+		}
 
-    // This kicks everything off
-    env.Jobqueue.Push(firstJob)
-    Loop(env)
+		chrm = chrome.NewChrome(env)
+		if chrm == nil {
+			log.Err("Failed to launch Chrome!")
+			return
+		}
+		go chrm.Loop(env)
+	} else {
+		log.Log(0, "Screenshotting is Disabled\n")
+		env.Cfg.DisableScreenShot = true
+	}
 
-    // This doesn't actually do much does it.
-    chrm.SignalDone()
+	// Spawn the Worker Threads
+	for i := uint(0); i < env.Cfg.NumWorkers; i++ {
+		go worker.Run(i, env)
+	}
 
-    // TODO: Findings to output dir
-    env.Targets.PrintFindings()
+	// This kicks everything off
+	env.Jobqueue.Push(firstJob)
+	Loop(env)
+
+	// This doesn't actually do much does it.
+	chrm.SignalDone()
+
+	// TODO: Findings to output dir
+	env.Targets.PrintFindings()
 }
-
--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -3,63 +3,60 @@
 package fs
 
 import (
-    "bufio"
-    "os"
-    "strings"
+	"bufio"
+	"os"
+	"strings"
 
-    "fafo/pkg/log"
+	"fafo/pkg/log"
 )
 
 func Exists(pathname string) bool {
-    if _, err := os.Stat(pathname); !os.IsNotExist(err) {
-        return true
-    }
-    return false
+	if _, err := os.Stat(pathname); !os.IsNotExist(err) {
+		return true
+	}
+	return false
 }
 
 func Mkdir(pathname string) error {
-    if !Exists(pathname) {
-        if err := os.MkdirAll(pathname, os.FileMode(0755)); err != nil {
-            return err
-        }
-    }
-    return nil
+	if !Exists(pathname) {
+		if err := os.MkdirAll(pathname, os.FileMode(0755)); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func Wc(filename string) (int, error) {
-    count := 0
-    file, err := os.Open(filename)
-    if err != nil {
-        return -1, err
-    }
+	count := 0
+	file, err := os.Open(filename)
+	if err != nil {
+		return -1, err
+	}
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        count++
-    }
-    file.Close()
-    return count, nil
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		count++
+	}
+	file.Close()
+	return count, nil
 }
 
 func chomp(str string) string {
-    if strings.HasSuffix(str, "\n") {
-        str = str[0:len(str) - 1]
-    }
-    return str
+	return strings.TrimSuffix(str, "\n")
 }
 
 func GetFileFromStdio(msg string) string {
-    var filename string
-    stdin := bufio.NewReader(os.Stdin)
-    for {
-        log.Logf(0, "%v: ", msg)
-        filename, _ = stdin.ReadString('\n')
-        filename = chomp(filename)
-        if Exists(filename) {
-            break
-        } else {
-            log.Logf(0, "File/directory \"%v\" does not exist! Try again?\n", filename)
-        }
-    }
-    return filename
+	var filename string
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		log.Logf(0, "%v: ", msg)
+		filename, _ = stdin.ReadString('\n')
+		filename = chomp(filename)
+		if Exists(filename) {
+			break
+		} else {
+			log.Logf(0, "File/directory \"%v\" does not exist! Try again?\n", filename)
+		}
+	}
+	return filename
 }
